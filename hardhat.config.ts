570336b1a928d664dab32ad351575f569ea65555
Sebastian Pape <s@pape.io>
2021-03-18 13:23:16 +0100

    install dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,13 +5,7 @@ import "hardhat-typechain";
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
-      { version: "0.7.5", settings: {} }, // for DePay
-      { version: "0.5.16", settings: {} }, // for Uniswap
-      { version: "0.6.0", settings: {} }, // for StakingPool
-      { version: "0.6.2", settings: {} }, // for StakingPool
-      { version: "0.6.6", settings: {} }, // for Uniswap
-      { version: "0.6.12", settings: {} }, // for StakingPool
-      { version: "0.4.18", settings: {} } // for WETH
+      { version: "0.7.6", settings: {} } // for DePayNFTAirdropV1
     ],
   }
 };
